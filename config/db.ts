0d0a5753dd182d071317d45615848b082f0f8788
Enzo Tiezzi <enzo.tiezzi@zellar.com.br>
2023-04-29 21:06:44 -0300

    connection String

--- config/db.ts
+++ config/db.ts
@@ -1,3 +1,3 @@
 export class DbConfig {
-    static readonly connectionString: string = "mongodb+srv://zymbar_dev:HdmSh7WwPXEF3ojB@zymbar.utmygv4.mongodb.net/test";
+    static readonly connectionString: string = process.env.CONNECTIONSTRING || "";
 }
