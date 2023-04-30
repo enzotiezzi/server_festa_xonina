63a8294b9f388cdcd5e63279b3b8dea09d6d5210
Enzo Tiezzi <enzo.tiezzi@zellar.com.br>
2023-04-29 21:11:33 -0300

    config folder import

--- index.ts
+++ index.ts
@@ -2,7 +2,6 @@ import * as express from 'express';
 import * as mongoose from 'mongoose';
 import * as cors from 'cors';
 import {Router} from './routes/routes';
-import { DbConfig } from './config/db';
 
 const app = express();
 const router = express.Router();
@@ -16,7 +15,7 @@ app.use(cors());
 Router.defineRoutes(app, router);
 
 mongoose
-    .connect(process.env.CONNECTIONSTRING || "")
+    .connect(process.env.CONNECTIONSTRING || "mongodb+srv://zymbar_dev:HdmSh7WwPXEF3ojB@zymbar.utmygv4.mongodb.net/test")
     .then(_ => {
         app.listen(port, () => {
             console.log("Servidor está UP");
