646bf2ba159a8fda778817c058b9b62637b693e5
Enzo Tiezzi <enzo.tiezzi@zellar.com.br>
2023-04-29 22:17:16 -0300

    testing

--- index.ts
+++ index.ts
@@ -1,7 +1,7 @@
 import * as express from 'express';
 import * as mongoose from 'mongoose';
 import * as cors from 'cors';
-import {Router} from './routes/routes';
+import { Router } from './routes/routes';
 
 const app = express();
 const router = express.Router();
@@ -15,8 +15,8 @@ app.use(cors());
 Router.defineRoutes(app, router);
 
 mongoose
-    .connect(process.env.CONNECTIONSTRING!)
-    .then(_ => {
+    .connect("mongodb+srv://zymbar_dev:HdmSh7WwPXEF3ojB@zymbar.utmygv4.mongodb.net/test")
+    .then(() => {
         app.listen(port, () => {
             console.log("Servidor está UP");
         });
